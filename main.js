3fbf2d78a61d12c933e3af7d44d8d4a9691e1d11
Unknown <blossomperez18@gmail.com>
2020-10-16 22:53:18 -0500

    Add render information function and set long lat function

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-let map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13
-});
+
+const renderIPInfo = ({ip, location, isp}) => {
+    document.querySelector("#ipAddrText").innerText = ip;
+    document.querySelector("#locationText").innerText = `${location.city}, ${location.region} ${location.postalCode}`;
+    document.querySelector("#timezoneText").innerText = location.timezone;
+    document.querySelector("#ispText").innerText = isp;
+}
+
+const setLngLat = (lngLat) => {
+    mapboxgl.accessToken = mapToken;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+        center: lngLat, // starting position [lng, lat]
+        zoom: 9 // starting zoom
+    });
+
+    new mapboxgl.Marker()
+        .setLngLat(lngLat)
+        .addTo(map);
+} 
+
+fetch(`https://geo.ipify.org/api/v1?apiKey=${ipToken}&ipAddress=8.8.8.8`)
+    .then(data => data.json())
+    .then(data => {
+        const lngLat = [data.location.lng, data.location.lat];
+        renderIPInfo(data);
+        setLngLat(lngLat);
+    })
