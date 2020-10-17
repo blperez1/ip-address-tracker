7a77e040a27c7bbf6af2051382b115807d757529
Unknown <blossomperez18@gmail.com>
2020-10-17 01:10:33 -0500

    Add DOMContentLoaded event on document

--- main.js
+++ main.js
@@ -1,65 +1,72 @@
+document.addEventListener("DOMContentLoaded", () => {
+    if(document.readyState === "complete" || document.readyState === "interactive") {
+        renderMap()
+    }
+})
 
-const abbrevateState = (state) => {
-let stateAbbrivations = {
-    "Alabama" : "AL",
-    "Alaska": "AK",
-    "Arizona": "AZ",
-    "Arkansas": "AR",
-    "California": "CA",
-    "Colorado": "CO",
-    "Connecticut": "CT",
-    "Delaware": "DE",
-    "Florida": "FL",
-    "Georgia": "GA",
-    "Hawaii": "HI",
-    "Idaho": "ID",
-    "Illinois": "IL",
-    "Indiana": "IN",
-    "Iowa": "IA",
-    "Kansas": "KS",
-    "Kentucky": "KY",
-    "Louisiana": "LA",
-    "Maine": "ME",
-    "Maryland": "MD",
-    "Massachusetts": "MA",
-    "Michigan": "MI",
-    "Minnesota": "MN",
-    "Mississippi": "MS",
-    "Missouri": "MO",
-    "Montana": "MT",
-    "Nebraska": "NE",
-    "Nevada": "NV",
-    "New Hampshire": "NH",
-    "New Jersey": "NJ",
-    "New Mexico": "NM",
-    "New York": "NY",
-    "North Carolina": "NC",
-    "North Dakota": "ND",
-    "Ohio": "OH",
-    "Oklahoma": "OK",
-    "Oregon": "OR",
-    "Pennsylvania": "PA",
-    "Rhode Island": "RI",
-    "South Carolina": "SC",
-    "South Dakota": "SD",
-    "Tennessee": "TN",
-    "Texas": "TX",
-    "Utah": "UT",
-    "Vermont": "VT",
-    "Virginia": "VA",
-    "Washington": "WA",
-    "West Virginia": "WV",
-    "Wisconsin": "WI",
-    "Wyoming": "WY"
+
+
+const abbreviateState = (state) => {
+    let Abbreviations = {
+        "Alabama" : "AL",
+        "Alaska": "AK",
+        "Arizona": "AZ",
+        "Arkansas": "AR",
+        "California": "CA",
+        "Colorado": "CO",
+        "Connecticut": "CT",
+        "Delaware": "DE",
+        "Florida": "FL",
+        "Georgia": "GA",
+        "Hawaii": "HI",
+        "Idaho": "ID",
+        "Illinois": "IL",
+        "Indiana": "IN",
+        "Iowa": "IA",
+        "Kansas": "KS",
+        "Kentucky": "KY",
+        "Louisiana": "LA",
+        "Maine": "ME",
+        "Maryland": "MD",
+        "Massachusetts": "MA",
+        "Michigan": "MI",
+        "Minnesota": "MN",
+        "Mississippi": "MS",
+        "Missouri": "MO",
+        "Montana": "MT",
+        "Nebraska": "NE",
+        "Nevada": "NV",
+        "New Hampshire": "NH",
+        "New Jersey": "NJ",
+        "New Mexico": "NM",
+        "New York": "NY",
+        "North Carolina": "NC",
+        "North Dakota": "ND",
+        "Ohio": "OH",
+        "Oklahoma": "OK",
+        "Oregon": "OR",
+        "Pennsylvania": "PA",
+        "Rhode Island": "RI",
+        "South Carolina": "SC",
+        "South Dakota": "SD",
+        "Tennessee": "TN",
+        "Texas": "TX",
+        "Utah": "UT",
+        "Vermont": "VT",
+        "Virginia": "VA",
+        "Washington": "WA",
+        "West Virginia": "WV",
+        "Wisconsin": "WI",
+        "Wyoming": "WY"
+    }
+    return Abbreviations[state];
 }
-    return stateAbbrivations[state];
-}
 
 
-const renderIPInfo = ({ip, location, isp}) => {
+const renderLocationInfo = ({ip, location, isp}) => {
     document.querySelector("#ipAddrText").innerText = ip;
-    document.querySelector("#locationText").innerText = `${location.city}, ${abbrevateState(location.region)} ${location.postalCode}`;
-    document.querySelector("#timezoneText").innerText = location.timezone;
+    document.querySelector("#locationText").innerText = `${location.city}, ${abbreviateState(location.region)} ${location.postalCode}`;
+    document.querySelector("#timezoneText").innerText = `UTC-${location.timezone}`;
     document.querySelector("#ispText").innerText = isp;
 }
 
@@ -77,10 +84,15 @@ const setLngLat = (lngLat) => {
         .addTo(map);
 } 
 
-fetch(`https://geo.ipify.org/api/v1?apiKey=${ipToken}&ipAddress=8.8.8.8`)
+const ipAddress = document.querySelector("#ipAddressInput").value;
+const btn = document.querySelector("#submitBtn");
+btn.addEventListener('click', () => {
+    fetch(`https://geo.ipify.org/api/v1?apiKey=${ipToken}&ipAddress=${ipAddress}`)
     .then(data => data.json())
     .then(data => {
         const lngLat = [data.location.lng, data.location.lat];
-        renderIPInfo(data);
+        renderLocationInfo(data);
         setLngLat(lngLat);
     })
+})
+
