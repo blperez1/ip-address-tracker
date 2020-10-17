87e494b4806795b3041eb8e0211d6304b15f8483
Unknown <blossomperez18@gmail.com>
2020-10-16 23:08:22 -0500

    Create state abbreviation object

--- main.js
+++ main.js
@@ -1,7 +1,64 @@
 
+const abbrevateState = (state) => {
+let stateAbbrivations = {
+    "Alabama" : "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY"
+}
+    return stateAbbrivations[state];
+}
+
+
 const renderIPInfo = ({ip, location, isp}) => {
     document.querySelector("#ipAddrText").innerText = ip;
-    document.querySelector("#locationText").innerText = `${location.city}, ${location.region} ${location.postalCode}`;
+    document.querySelector("#locationText").innerText = `${location.city}, ${abbrevateState(location.region)} ${location.postalCode}`;
     document.querySelector("#timezoneText").innerText = location.timezone;
     document.querySelector("#ispText").innerText = isp;
 }
